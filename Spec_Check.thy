bf2669877811769f5790518b6561ee5fadc948af
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2020-12-18 15:12:48 +0100

    Initial commit for Spec_Check

---
+++ Spec_Check.thy
@@ -0,0 +1,15 @@
+theory Spec_Check
+imports Pure
+begin
+
+ML_file \<open>random.ML\<close>
+ML_file \<open>property.ML\<close>
+ML_file \<open>gen_base.ML\<close>
+ML_file \<open>gen_datatypes.ML\<close>
+ML_file \<open>gen_term.ML\<close>
+ML_file \<open>generator.ML\<close>
+ML_file \<open>gen_construction.ML\<close>
+ML_file \<open>spec_check.ML\<close>
+ML_file \<open>output_style.ML\<close>
+
+end
