721073b75f2e5cdfdf5b436723ac80944cf3c2d2
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2021-01-14 17:12:54 +0100

    Update dependency graph another time

--- src/Spec_Check.thy
+++ src/Spec_Check.thy
@@ -8,10 +8,10 @@ ML_file \<open>gen_base.ML\<close>
 ML_file \<open>gen_datatypes.ML\<close>
 ML_file \<open>gen_term.ML\<close>
 ML_file \<open>generator.ML\<close>
-ML_file \<open>gen_construction.ML\<close>
 ML_file \<open>configuration.ML\<close>
 ML_file \<open>output_style.ML\<close>
 ML_file \<open>spec_check.ML\<close>
+ML_file \<open>gen_construction.ML\<close>
 ML_file \<open>spec_check_comp.ML\<close>
 
 end
