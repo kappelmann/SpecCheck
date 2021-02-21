4f30779a94f87c97ad965598a155498ca4aec568
Kevin Kappelmann <accounts@kappelmann.me>
2021-02-21 21:22:59 +0100

    feat(*) refactor files, increase modularity

--- src/Spec_Check.thy
+++ src/Spec_Check.thy
@@ -2,17 +2,30 @@ theory Spec_Check
 imports Pure
 begin
 
-ML_file \<open>utils.ML\<close>
-ML_file \<open>random.ML\<close>
+ML_file \<open>util.ML\<close>
+
 ML_file \<open>property.ML\<close>
-ML_file \<open>gen_base.ML\<close>
-ML_file \<open>gen_datatypes.ML\<close>
-ML_file \<open>gen_term.ML\<close>
-ML_file \<open>generator.ML\<close>
 ML_file \<open>configuration.ML\<close>
-ML_file \<open>output_style.ML\<close>
+
+ML_file \<open>output_styles/output_style_types.ML\<close>
+ML_file \<open>output_styles/output_style_perl.ML\<close>
+ML_file \<open>output_styles/output_style_cm.ML\<close>
+ML_file \<open>output_styles/output_style_custom.ML\<close>
+ML_file \<open>output_styles/output_style.ML\<close>
+
+ML_file \<open>random.ML\<close>
+
+ML_file \<open>generators/gen_types.ML\<close>
+ML_file \<open>generators/gen_base.ML\<close>
+ML_file \<open>generators/gen_text.ML\<close>
+ML_file \<open>generators/gen_int.ML\<close>
+ML_file \<open>generators/gen_real.ML\<close>
+ML_file \<open>generators/gen_function.ML\<close>
+ML_file \<open>generators/gen_term.ML\<close>                             
+ML_file \<open>generators/gen.ML\<close>
+
 ML_file \<open>spec_check.ML\<close>
-ML_file \<open>gen_construction.ML\<close>
+ML_file \<open>construct_gen.ML\<close>
 ML_file \<open>spec_check_comp.ML\<close>
 
 end
